5da296086826aa16db80f6a1a8d01679a6f0bd9c
moy <fabmoy1866@gmail.com>
2020-12-03 12:33:58 -0600

    Terminada estructura y estilos header y nav

--- appweb/mod/header.php
+++ appweb/mod/header.php
@@ -1,1 +1,33 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a href="/index.php" class="navbar-brand">
+        <img class="imgNav d-block" width="140" src="/appweb/images/logoNav.png">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link h5" href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link h5" href="#">Tienda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link h5" href="#">Contactanos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link h5" href="#">Conocenos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link h5" href="#">Ayuda</a>
+            </li>
+
+        </ul>
+        <a href="#" id="carritoBtn"><img src="/appweb/images/carticon.svg"></a>
+        <button class="btn btn-primary" type="button" id="botonLogin">Login</button>
+    </div>
+
+
+
+</nav>
