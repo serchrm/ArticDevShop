737d82609f386065f44ee6090f1e308ab4145276
moy <fabmoy1866@gmail.com>
2020-12-08 23:27:32 -0600

    Modificada pantalla home

--- appweb/mod/mainShop.php
+++ appweb/mod/mainShop.php
@@ -8,7 +8,8 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/appweb/css/styles.css">
-    <link rel="stylesheet" href="/appweb/css/mainShopstyles.css">
+    <link rel="stylesheet" href="/appweb/css/mainShopStyles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
@@ -80,7 +81,6 @@
     </div>';*/
         //GENERAR AREA PRODUCTOS
     echo "<div id='areaProductos'>";
-    
     if (!isset($_GET["categ"])||empty($_GET["categ"])){
         $categ="*";
     }else{
--- index.php
+++ index.php
@@ -8,38 +8,37 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="appweb/css/styles.css">
+        <link rel="stylesheet" href="appweb/css/indexStyles.css">
         <script src="https://kit.fontawesome.com/791abd0481.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
     </head>
 
     <body>
         <?php require_once "appweb/inc/initialconfig.php";?>
         <?php include "appweb/mod/header.php" ?>
-        <div class="slideshow-container">
-
-            <div class="mySlides fade">
-                <img src="appweb/images/promo2.jpg" style="width:1200px; height: 450px;">
+        <div id="initPage">
+            <div id="init1">
+                <div id="imagenTarj">
+                    <div class="tarj" id="im1">
+                        <h3>RTX 2060</h3>
+                    </div>
+                    <div class="tarj" id="im2">
+                        <h3>RX VEGA</h3>
+                    </div>
+                    <div class="tarj" id="im3">
+                        <h3>5500 XT</h3>
+                    </div>
+                </div>
             </div>
-
-            <div class="mySlides fade">
-                <img src="appweb/images/promo1.jpg" style="width:1200px; height: 450px;">
-            </div>
-
-            <div class="mySlides fade">
-                <img src="appweb/images/promo3.jpg" style="width:1200px; height: 450px;">
-            </div>
+            <h1>Elige tu favorita</h1>
 
         </div>
-        <br>
 
-        <div class="punto">
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
-        </div>
-        <div>
-            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis totam voluptatibus excepturi eum corrupti est neque iure veritatis harum modi aliquam non porro nesciunt nostrum, repudiandae mollitia quisquam. Quidem, ea!</span><span>Ut assumenda temporibus nisi vero odit quae, numquam laudantium, modi corrupti totam ipsam optio sequi quaerat! Perspiciatis ullam totam voluptates, voluptatum quisquam quaerat, a tempora inventore officiis perferendis, ea soluta.</span></p>
-        </div>
         <div class="contenedor">
             <div class="img-accordion">
                 <div class="img img1">
@@ -60,7 +59,6 @@
             </div>
         </div>
 
-        <script src="appweb/js/carrusel.js"></script>
         <?php include "appweb/mod/footer.php";?>
         <script src="https://kit.fontawesome.com/791abd0481.js" crossorigin="anonymous"></script>
     </body>
