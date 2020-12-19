59f2122e19f7f56cf118573db96dd00cc38bb026
moy <fabmoy1866@gmail.com>
2020-12-18 21:37:24 -0600

    Bug corregido

--- appweb/mod/registroLogica.php
+++ appweb/mod/registroLogica.php
@@ -6,7 +6,7 @@ $email = trim (strtolower($_POST["emailReg"]));
 $passwr= $_POST["contrasReg"];
 $nickname=trim($_POST["nombUsuar"]);
 
-if($conexionBD->query("INSERT INTO usuario (nombre,email,pass_md5,nickname,blocked) VALUES('".$nombre."','".$email."',MD5('".$passwr."'),'".$nickname."',false)")){
+if($conexionBD->query("INSERT INTO usuario (nombre,email,pass_md5,nickname,intentos) VALUES('".$nombre."','".$email."',MD5('".$passwr."'),'".$nickname."',0)")){
     $ruta="Location: ../../index.php";
 }else{
     $ruta="Location: errorScreen.php?error=".$conexionBD->error;
